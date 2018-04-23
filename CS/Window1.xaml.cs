716a5aa844f2f6a9bc18aa78fdd2259b3183726a
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Window1.xaml.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows;
+
+namespace Area2DChart {
+
+    public partial class Window1 : Window {
+        public Window1() {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to create a 2D Area chart
+
+
+<p>The following example demonstrates how to create a 2D Area chart.</p>
+
+<br/>
+
+
