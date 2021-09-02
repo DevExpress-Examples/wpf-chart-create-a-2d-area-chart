7fa1df2bdc0662cd591fb8236a1fb87b8eb0556b
DevExpressExampleBot <fakeEmail>
2021-09-02 18:33:56 +0300

    Source auto update [skip ci]

--- VB/Window1.xaml.vb
+++ VB/Window1.xaml.vb
@@ -26,23 +26,23 @@ Namespace Area2DChart
 					New DataSeries With {
 						.Name = "DevAV North",
 						.Values = New ObservableCollection(Of DataPoint) From {
-							New DataPoint(New Date(2013,12,31), 362.5),
-							New DataPoint(New Date(2014,12,31), 348.4),
-							New DataPoint(New Date(2015,12,31), 279.0),
-							New DataPoint(New Date(2016,12,31), 230.9),
-							New DataPoint(New Date(2017,12,31), 203.5),
-							New DataPoint(New Date(2018,12,31), 197.1)
+							New DataPoint(New DateTime(2013,12,31), 362.5),
+							New DataPoint(New DateTime(2014,12,31), 348.4),
+							New DataPoint(New DateTime(2015,12,31), 279.0),
+							New DataPoint(New DateTime(2016,12,31), 230.9),
+							New DataPoint(New DateTime(2017,12,31), 203.5),
+							New DataPoint(New DateTime(2018,12,31), 197.1)
 						}
 					},
 					New DataSeries With {
 						.Name = "DevAV South",
 						.Values = New ObservableCollection(Of DataPoint) From {
-							New DataPoint(New Date(2013,12,31), 277.0),
-							New DataPoint(New Date(2014,12,31), 328.5),
-							New DataPoint(New Date(2015,12,31), 297.0),
-							New DataPoint(New Date(2016,12,31), 255.3),
-							New DataPoint(New Date(2017,12,31), 173.5),
-							New DataPoint(New Date(2018,12,31), 131.8)
+							New DataPoint(New DateTime(2013,12,31), 277.0),
+							New DataPoint(New DateTime(2014,12,31), 328.5),
+							New DataPoint(New DateTime(2015,12,31), 297.0),
+							New DataPoint(New DateTime(2016,12,31), 255.3),
+							New DataPoint(New DateTime(2017,12,31), 173.5),
+							New DataPoint(New DateTime(2018,12,31), 131.8)
 						}
 					}
 				}
@@ -52,9 +52,9 @@ Namespace Area2DChart
 			Public Property Values() As ObservableCollection(Of DataPoint)
 		End Class
 		Public Class DataPoint
-			Public Property Argument() As Date
+			Public Property Argument() As DateTime
 			Public Property Value() As Double
-			Public Sub New(ByVal argument As Date, ByVal value As Double)
+			Public Sub New(ByVal argument As DateTime, ByVal value As Double)
 				Me.Argument = argument
 				Me.Value = value
 			End Sub
