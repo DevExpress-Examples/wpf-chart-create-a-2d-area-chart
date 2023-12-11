083718611558d610fb43b8f17cd7a471e7f0c214
DevExpressExampleBot <fakeEmail>
2023-12-11 18:38:05 +0400

    Source auto update [skip ci]

--- VB/Window1.xaml.vb
+++ VB/Window1.xaml.vb
@@ -1,12 +1,12 @@
-﻿Imports Microsoft.VisualBasic
 Imports System.Windows
 
 Namespace Area2DChart
 
-	Partial Public Class Window1
-		Inherits Window
-		Public Sub New()
-			InitializeComponent()
-		End Sub
-	End Class
+    Public Partial Class Window1
+        Inherits Window
+
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
+    End Class
 End Namespace
